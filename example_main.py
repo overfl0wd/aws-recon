c72810e714dbe758c1700de844861306821ed7b9
Dana Taylor <danataylor.ct@gmail.com>
2019-03-26 17:07:19 -0400

    tidying up example

--- example_main.py
+++ example_main.py
@@ -1,18 +1,14 @@
 from aws_recon import AwsSession
 
-
-test_class = AwsSession("production", "us-east-1")
-
-""" Initialize the AWS Session class above.
+""" Initialize the AWS Session class.
 Pass a profile name from your configs (~/.aws/),
 as well as a region to use.
 """
+prod_east1 = AwsSession("production", "us-east-1")
 
-test_class.enumerate_servers()
-test_class.enumerate_securitygroups()
 
-print("\n### ec2 instances ###\n")
-print(test_class.servers)
-
-print("\n### security groups ###\n")
-print(test_class.securitygroups)
+""" Run the EC2 instance enumeration function,
+and print the returned information.
+"""
+prod_east1.enumerate_servers()
+print(prod_east1.servers)
