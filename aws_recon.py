ed84daa37af05004b0c1621a2454d63c24c1a9bc
Dana Taylor <danataylor.ct@gmail.com>
2019-03-26 17:28:06 -0400

    adding network interface enumeration

--- aws_recon.py
+++ aws_recon.py
@@ -9,6 +9,7 @@ class AwsSession:
 		self.resource = self._setup_resource()
 		self.servers = {}
 		self.securitygroups = {}
+		self.networkinterfaces = {}
 
 
 	def _setup_session(self):
@@ -23,7 +24,9 @@ class AwsSession:
 		_resource = self.session.resource("ec2")
 		return _resource
 
+
 	def enumerate_servers(self):
+
 		""" Queries all EC2 instances in the session.
 		Returns all associated tags, private + public IPs, and current state:
 		(running / stopped / terminated / etc).
@@ -34,14 +37,14 @@ class AwsSession:
 			_secondaries = []
 
 			try:  # Grab all tags, add them to the dictionary. Passes if there are none.
-				for tags in _server.tags:
-					_tags["Tags"][tags["Key"]] = tags["Value"]
+				for _tag in _server.tags:
+					_tags["Tags"][_tag["Key"]] = _tag["Value"]
 			except TypeError:
 				pass
 
 			try:  # Grab all secondary IP addresses, and pass if there are none.
-				for item in _server.network_interfaces_attribute[0]["PrivateIpAddresses"]:
-					_secondaries.append(item["PrivateIpAddress"])
+				for _address in _server.network_interfaces_attribute[0]["PrivateIpAddresses"]:
+					_secondaries.append(_address["PrivateIpAddress"])
 			except IndexError:
 				pass
 
@@ -58,8 +61,9 @@ class AwsSession:
 			)
 
 	def enumerate_securitygroups(self):
+
 		""" Queries all security groups in the session.
-		Returns group name, description, VPC, and ID.
+		Returns ID, name, description, and associated VPC.
 		"""
 
 		for _group in self.resource.security_groups.all():
@@ -67,4 +71,46 @@ class AwsSession:
 				{"Name": _group.group_name},
 				{"Description": _group.description},
 				{"VPC": _group.vpc_id}
+			)
+
+	def enumerate_networkinterfaces(self):
+
+		""" Queries all network interfaces in the session.
+		Returns ID, public addresses, private addresses, and tags.
+		"""
+
+		for _interface in self.resource.network_interfaces.all():
+			_tags = { "Tags": {}}
+			_secondaries = []
+
+			try:  # Grab all tags, add them to the dictionary. Passes if there are none.
+				for _tag in _interface.tag_set:
+					_tags["Tags"][_tag["Key"]] = _tag["Value"]
+			except KeyError:
+				pass
+
+			try:  # Grab all secondary IP addresses, and pass if there are none.
+				for _address in _interface.private_ip_addresses:
+					_secondaries.append(_address["PrivateIpAddress"])
+			except IndexError:
+				pass
+
+			try: # Grab public DNS name, or set to empty and pass if one isn't found.
+				_publicdns = _interface.private_ip_addresses[0]["Association"]["PublicDnsName"]
+			except KeyError:
+				_publicdns = ""
+				pass
+
+			try: # Grab public IP address, or set to empty and pass if one isn't found.
+				_publicip = _interface.private_ip_addresses[0]["Association"]["PublicIp"]
+			except KeyError:
+				_publicip = ""
+				pass
+
+			self.networkinterfaces[_interface.network_interface_id] = (
+				{"Public IP Address": _publicip},
+				{"Public DNS Name": _publicdns},
+				{"Current Private IP": _interface.private_ip_address},
+				{"Available Private IPs": _secondaries},
+				_tags
 			)
