24733e7bc19715f0ccfe434d6964b630ddbd05cf
Dana Taylor <danataylor.ct@gmail.com>
2019-04-16 12:25:46 -0400

    changed json output to list of dicts

--- aws_recon.py
+++ aws_recon.py
@@ -10,12 +10,12 @@ class AwsSession:
 		self.elbclient = self._setup_elb_client()
 		self.albclient = self._setup_alb_client()
 		self.resource = self._setup_resource()
-		self.servers = {}
-		self.securitygroups = {}
-		self.networkinterfaces = {}
-		self.classiclbs = {}
-		self.applicationlbs = {}
-		self.elasticips = {}
+		self.servers = []
+		self.securitygroups = []
+		self.networkinterfaces = []
+		self.classiclbs = []
+		self.applicationlbs = []
+		self.elasticips = []
 
 
 	def _setup_session(self):
@@ -51,30 +51,18 @@ class AwsSession:
 
 		for _server in self.resource.instances.all():
 			_secondaries = []
-
-			self.servers[_server.instance_id] = {
-				"Profile": self.profile,
-				"Region": self.region,
-				"Service": "EC2",
-				"Resource": "Instance",
-				"Instance ID": _server.instance_id,
-				"Current Private IP": _server.private_ip_address,
-				"Public IP": _server.public_ip_address,
-				"State": _server.state["Name"],
-				"AMI": _server.image_id,
-				"Instance Type": _server.instance_type,
-				"Key Name": _server.key_name,
-			}
+			_tags = {}
 
 			try:  # Get all tags, add them to the dictionary. Passes if there are none.
 				for _tag in _server.tags:
-					self.servers[_server.instance_id][_tag["Key"]] = _tag["Value"]
+					_tags[_tag["Key"]] = _tag["Value"]
 			except TypeError:
 				pass
 
 			try:  # Get the IAM role attached to the instance.
-				self.servers[_server.instance_id]["IAM Role"] = _server.iam_instance_profile["Arn"]
+				_iamrole = _server.iam_instance_profile["Arn"]
 			except TypeError:
+				_iamrole = None
 				pass
 
 			try:  # Get all secondary IP addresses, and pass if there are none.
@@ -83,9 +71,28 @@ class AwsSession:
 			except IndexError:
 				pass
 
-			self.servers[_server.instance_id]["Available Private IPs"] = ', '.join(_secondaries)
+			_secondaries = ', '.join(_secondaries)
+
+			self.servers.append({
+				"Profile": self.profile,
+				"Region": self.region,
+				"Service": "EC2",
+				"Resource": "Instance",
+				"ID": _server.instance_id,
+				"Private IP": _server.private_ip_address,
+				"Public IP": _server.public_ip_address,
+				"State": _server.state["Name"],
+				"AMI": _server.image_id,
+				"Instance Type": _server.instance_type,
+				"Key Name": _server.key_name,
+				"IAM Role": _iamrole,
+				"Tags": _tags,
+				"Secondary IPs": _secondaries
+			})
 
 		self.servers = json.dumps(self.servers, indent=2)
+
+		return(self.servers)
 
 	def enumerate_securitygroups(self):
 
@@ -95,25 +102,29 @@ class AwsSession:
 		"""
 
 		for _group in self.resource.security_groups.all():
+			_tags = {}
 
-			self.securitygroups[_group.group_id] = {
+			try:  # Get all tags, add them to the dictionary. Passes if there are none.
+				for _tag in _group.tags:
+					_tags[_tag["Key"]] = _tag["Value"]
+			except TypeError:
+				pass
+
+				self.securitygroups.append({
 				"Profile": self.profile,
 				"Region": self.region,
 				"Service": "EC2",
 				"Resource": "Security Group",
 				"Name": _group.group_name,
-				"Group ID": _group.group_id,
+				"ID": _group.group_id,
 				"Description": _group.description,
-				"VPC": _group.vpc_id
-			}
-
-			try:  # Get all tags, add them to the dictionary. Passes if there are none.
-				for _tag in _group.tags:
-					self.securitygroups[_group.group_id][_tag["Key"]] = _tag["Value"]
-			except TypeError:
-				pass
+				"VPC": _group.vpc_id,
+				"Tags": _tags
+			})
 
 		self.securitygroups = json.dumps(self.securitygroups, indent=2)
+
+		return(self.securitygroups)
 
 	def enumerate_networkinterfaces(self):
 
@@ -124,30 +135,24 @@ class AwsSession:
 		"""
 
 		for _interface in self.resource.network_interfaces.all():
+			_tags = {}
 			_secondaries = []
 
-			self.networkinterfaces[_interface.network_interface_id] = {
-				"Profile": self.profile,
-				"Region": self.region,
-				"Service": "EC2",
-				"Resource": "Network Interface",
-				"Interface ID": _interface.network_interface_id,
-				"Current Private IP": _interface.private_ip_address
-			}
-
 			try:  # Get public IP address, or set to empty and pass if one isn't found.
-				self.networkinterfaces[_interface.network_interface_id]["Public IP Address"] = _interface.private_ip_addresses[0]["Association"]["PublicIp"]
+				_publicip = _interface.private_ip_addresses[0]["Association"]["PublicIp"]
 			except KeyError:
+				_publicip = None
 				pass
 
 			try:  # Get public DNS name, or set to empty and pass if one isn't found.
-				self.networkinterfaces[_interface.network_interface_id]["Public DNS Name"] = _interface.private_ip_addresses[0]["Association"]["PublicDnsName"]
+				_dnsname = _interface.private_ip_addresses[0]["Association"]["PublicDnsName"]
 			except KeyError:
+				_dnsname = None
 				pass
 
 			try:  # Get all tags, add them to the dictionary. Passes if there are none.
 				for _tag in _interface.tag_set:
-					self.networkinterfaces[_interface.network_interface_id][_tag["Key"]] = _tag["Value"]
+					_tags[_tag["Key"]] = _tag["Value"]
 			except KeyError:
 				pass
 
@@ -157,9 +162,24 @@ class AwsSession:
 			except IndexError:
 				pass
 
-			self.networkinterfaces[_interface.network_interface_id]["Available Private IPs"] = ', '.join(_secondaries)
+			_secondaries = ', '.join(_secondaries)
+
+			self.networkinterfaces.append({
+				"Profile": self.profile,
+				"Region": self.region,
+				"Service": "EC2",
+				"Resource": "Network Interface",
+				"ID": _interface.network_interface_id,
+				"Private IP": _interface.private_ip_address,
+				"Public IP": _publicip,
+				"DNS Name": _dnsname,
+				"Tags": _tags,
+				"Secondary IPs": _secondaries
+			})
 
 		self.networkinterfaces = json.dumps(self.networkinterfaces, indent=2)
+
+		return(self.networkinterfaces)
 
 	def enumerate_classiclbs(self):
 
@@ -177,17 +197,6 @@ class AwsSession:
 			_attachments = []
 			_securitygroups = []
 
-			self.classiclbs[_lb["LoadBalancerName"]] = {
-				"Profile": self.profile,
-				"Region": self.region,
-				"Service": "EC2",
-				"Resource": "Load Balancer",
-				"Type": "classic",
-				"Scheme": _lb["Scheme"],
-				"Name": _lb["LoadBalancerName"],
-				"DNS Name": _lb["DNSName"]
-			}
-
 			for _listener in _lb["ListenerDescriptions"]:  # Get the listening ports and protocols, add to dict.
 				_listeners[_listener["Listener"]["LoadBalancerPort"]] = _listener["Listener"]["Protocol"]
 
@@ -200,11 +209,27 @@ class AwsSession:
 			except KeyError:
 				pass
 
-			self.classiclbs[_lb["LoadBalancerName"]]["Attached Servers"] = ', '.join(_attachments)
-			self.classiclbs[_lb["LoadBalancerName"]]["Security Groups"] = ', '.join(_securitygroups)
-			self.classiclbs[_lb["LoadBalancerName"]]["Listeners"] = _listeners
+			_attachments = ', '.join(_attachments)
+			_securitygroups = ', '.join(_securitygroups)
+			_listeners = _listeners
+
+			self.classiclbs.append({
+				"Profile": self.profile,
+				"Region": self.region,
+				"Service": "EC2",
+				"Resource": "Load Balancer",
+				"Type": "classic",
+				"Scheme": _lb["Scheme"],
+				"Name": _lb["LoadBalancerName"],
+				"DNS Name": _lb["DNSName"],
+				"Attached Servers": _attachments,
+				"Security Groups": _securitygroups,
+				"Listeners": _listeners
+			})
 
 		self.classiclbs = json.dumps(self.classiclbs, indent=2)
+
+		return(self.classiclbs)
 
 	def enumerate_applicationlbs(self):
 
@@ -219,7 +244,15 @@ class AwsSession:
 		for _lb in _response["LoadBalancers"]:
 			_securitygroups = []
 
-			self.applicationlbs[_lb["LoadBalancerName"]] = {
+			try:  # Get attached security groups, and pass if there aren't any.
+				for _group in _lb["SecurityGroups"]:
+					_securitygroups.append(_group)
+			except KeyError:
+				pass
+
+			_securitygroups = ', '.join(_securitygroups)
+
+			self.applicationlbs.append({
 				"Profile": self.profile,
 				"Region": self.region,
 				"Service": "EC2",
@@ -227,18 +260,13 @@ class AwsSession:
 				"Name": _lb["LoadBalancerName"],
 				"Type": _lb["Type"],
 				"Scheme": _lb["Scheme"],
-				"DNS Name": _lb["DNSName"]
-			}
-
-			try:  # Get attached security groups, and pass if there aren't any.
-				for _group in _lb["SecurityGroups"]:
-					_securitygroups.append(_group)
-			except KeyError:
-				pass
-
-			self.applicationlbs[_lb["LoadBalancerName"]]["Security Groups"] = ', '.join(_securitygroups)
+				"DNS Name": _lb["DNSName"],
+				"Security Groups": _securitygroups
+			})
 
 		self.applicationlbs = json.dumps(self.applicationlbs, indent=2)
+
+		return(self.applicationlbs)
 
 	def enumerate_elasticips(self):
 
@@ -251,23 +279,27 @@ class AwsSession:
 		_response = self.resource.vpc_addresses.all()
 
 		for _address in _response:
+			_tags = {}
 
-			self.elasticips[_address.allocation_id] = {
+			try:  # Get all tags, add them to the dictionary. Passes if there are none.
+				for _tag in _address.tags:
+					_tags[_tag["Key"]] = _tag["Value"]
+			except TypeError:
+				pass
+
+			self.elasticips.append({
 				"Profile": self.profile,
 				"Region": self.region,
 				"Service": "EC2",
 				"Resource": "Elastic IP",
-				"Allocation ID": _address.allocation_id,
+				"ID": _address.allocation_id,
 				"Attached Instance": _address.instance_id,
 				"Network Interface":_address.network_interface_id,
 				"Private IP": _address.private_ip_address,
-				"Public IP": _address.public_ip
-			}
-
-			try:  # Get all tags, add them to the dictionary. Passes if there are none.
-				for _tag in _address.tags:
-					self.elasticips[_address.allocation_id][_tag["Key"]] = _tag["Value"]
-			except TypeError:
-				pass
+				"Public IP": _address.public_ip,
+				"Tags": _tags
+			})
 
 		self.elasticips = json.dumps(self.elasticips, indent=2)
+
+		return(self.elasticips)
